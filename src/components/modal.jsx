7cb111ed19704195b1de4d5c35d8f5bb5cd6892a
Ahmed <ahmedfdez93@gmail.com>
2024-08-01 23:29:10 -0400

    Slice updated. handleModalClose fn add

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,10 +1,22 @@
 import styles from "../assets/styles/modal.module.css"
+import { IoMdClose } from "react-icons/io";
+import { useDispatch } from "react-redux"
+import { hideModal } from "../redux/modalSlice"
 
 export const ModalWindow = () => {
+    const dispatch = useDispatch()
+    
+    const handleModalClose = () => {
+        dispatch(hideModal())
+    }
+    
+
     return <>
         <section className={styles['modal-container']}>
             <div className={styles['modal-task']}>
-                <h1>Add Task</h1>
+                <div id={styles['close']}>
+                    <IoMdClose id={styles['close-btn']} onClick={handleModalClose}/>
+                </div>
             </div>
         </section>
     </>
