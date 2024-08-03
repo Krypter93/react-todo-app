7809fe1f444202e2352ffc4c3840b303cbc31311
Ahmed <ahmedfdez93@gmail.com>
2024-08-03 02:04:23 -0400

    setting up UI of modal elements

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,6 +2,7 @@ import styles from "../assets/styles/modal.module.css"
 import { IoMdClose } from "react-icons/io";
 import { useDispatch } from "react-redux"
 import { hideModal } from "../redux/modalSlice"
+import { FaCheck } from "react-icons/fa";
 
 export const ModalWindow = () => {
     const dispatch = useDispatch()
@@ -17,7 +18,25 @@ export const ModalWindow = () => {
                 <div id={styles['close']}>
                     <IoMdClose id={styles['close-btn']} onClick={handleModalClose}/>
                 </div>
+
+                <div id={styles['modal-info']}>
+                    <fieldset>
+                        <label htmlFor="task">
+                            <input type="text" id={styles['task']} placeholder="Add your task" />
+                        </label>
+                        <label htmlFor="category">
+                            <select name="category" id={styles['category']}>
+                                <option value="Category">Category</option>
+                                <option value="Complete">Complete</option>
+                                <option value="Incomplete">Incomplete</option>
+                            </select>
+                        </label>
+                        <button><FaCheck /></button>
+                    </fieldset>
+                </div>
             </div>
+
+            
         </section>
     </>
 }
