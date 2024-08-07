d31a9bbeb6289918c620ca56e2d8980c07575d7b
Ahmed <ahmedfdez93@gmail.com>
2024-08-07 12:54:18 -0400

    Add complete UI of task View and working on fn to handle deletion of tasks

--- src/components/TaskView.jsx
+++ src/components/TaskView.jsx
@@ -1,10 +1,17 @@
 import styles from "../assets/styles/taskView.module.css"
 import PropTypes from 'prop-types'
+import { MdDelete } from "react-icons/md";
 
 export const TaskView = ({mainSelect}) => {
     const taskStorage = JSON.parse(localStorage.getItem('tasks')) || []
     const tasks = taskStorage.filter(task => task.category === mainSelect)
     console.log(taskStorage)
+
+    const handelDeleteTask = (id) => {
+        const newTasks = taskStorage.filter(task => task.id !== id)
+        localStorage.setItem('tasks', JSON.stringify(newTasks))
+    }
+
     return <>
         <section className={styles['task-view']}>
             {taskStorage.length < 1 ?
@@ -17,6 +24,7 @@ export const TaskView = ({mainSelect}) => {
                     return <li key={task.id}>
                         <p>{task.description}</p>
                         <p>{task.createdAt}</p>
+                        <MdDelete id={styles['delete-icon']} onClick={() => handelDeleteTask(task.id)}/>
                     </li>
                 })}
              </ul>
